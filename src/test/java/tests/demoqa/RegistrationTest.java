ef384c5f8f9c8e58d1f3b0e16bfcbf37890e4e07
Roman Kravchenko <roman.me@mail.ru>
2022-05-12 17:26:21 +0300

    initial commit

---
+++ src/test/java/tests/demoqa/RegistrationTest.java
@@ -0,0 +1,61 @@
+package tests.demoqa;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+@Tag("demoqa")
+
+public class RegistrationTest {
+
+    @BeforeAll
+    static void preconditions() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+    @Test
+    void fillFormTest() {
+        step("Открытие формы регистрации", () -> {
+            open("automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        });
+        step("Заполнение формы регистрации", () -> {
+            $("#firstName").setValue("Джон");
+            $("#lastName").setValue("Конар");
+            $("#userEmail").setValue("roman.mi@mail.ru");
+            $("#genterWrapper").$(byText("Other")).click();
+            $("#userNumber").setValue("1231231230");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__year-select").selectOption("2005");
+            $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("img/1.png");
+            $("#currentAddress").setValue("Some street 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
+        step("Проверить данные формы", () -> {
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text("Джон Конар"), text("roman.mi@mail.ru"), text("Other"));
+        });
+
+
+    }
+
+
+}
